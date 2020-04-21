f0ca542c5aca94692e2c96f96a82023b019d4d45
BadKiko <kiko.games.lc@gmail.com>
2020-04-21 19:39:44 +0300

    Не работает...

---
+++ Game/Scripts/LSSScripts/Bind.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bind : MonoBehaviour
+{
+    [SerializeField] private LSS.LSS_FrontEnd LseseScript;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            LseseScript.Load("Example2_On");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            LseseScript.Load("Example2_Off");
+        }
+    }
+}
---
+++ Game/Scripts/OptimizationScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class OptimizationScript : MonoBehaviour
+{
+    [SerializeField] private GameObject[] HideAndShowObjects;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Hide Objects");
+            HideAndShowObjects[0].SetActive(false);
+            HideAndShowObjects[1].SetActive(false);
+            HideAndShowObjects[2].SetActive(false);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Show Objects");
+            HideAndShowObjects[0].SetActive(true);
+            HideAndShowObjects[1].SetActive(true);
+            HideAndShowObjects[2].SetActive(true);
+        }
+    }
+}
