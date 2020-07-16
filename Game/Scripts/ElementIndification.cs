294b21f2aafdf7e75a758a10e67e28f4997eb024
BadKiko <53761278+BadKiko@users.noreply.github.com>
2020-07-17 01:19:45 +0300

    Добавление лития №3

--- Game/Scripts/ElementIndification.cs
+++ Game/Scripts/ElementIndification.cs
@@ -14,9 +14,20 @@ public class ElementIndification : MonoBehaviour
     [SerializeField] Color ColorWithoutOxygen, ColorWithOxygen;
     [Header("Скорость окисления:")]
     [SerializeField] private float OxygenSpeed;
+    [Header("Окисление:")]
+    [Header("Какой эффект пременить к объекту? :")]
+    [SerializeField] private bool Oxidation = false;
 
 
+    [Header("Объект с которым сталкивается наш объект:")]
+    [SerializeField] private GameObject CollisionObject;
 
+    [Header("Эффект который добавляется при окислении: ")]
+    [SerializeField] private GameObject BubbleGameObject;
+
+
+    private Vector3 BeginLerp;
+    private Vector3 FinishLerp;
     //[Header("В жидкости ли металл?")]
     //[SerializeField] private bool MetallInFluid;
     private void Start()
@@ -42,7 +53,25 @@ public class ElementIndification : MonoBehaviour
 
     private void LithiumInFluid()
     {
+        Oxidation = true; // Ставим эффект окисления при столкновении с водой
 
+        this.gameObject.transform.localRotation.eulerAngles.Set(0, 0, 0); // Ставим zero чтобы все камни лерпались по одинаковому
+
+        Instantiate(BubbleGameObject, this.gameObject.transform); // Создаеем пузырение а затем ставим на false чтобы не создавать много.
+
+        this.gameObject.AddComponent<StoneInLiquid>(); // Добовляем нашему камню при столкновении скрипт связанный с водой.
+                                                       // Он будет контроллировать высоту камню чтобы камень плавал на поверхности воды.
+
+        this.gameObject.GetComponent<StoneInLiquid>().Stone = this.gameObject; // Даем скрипту наш камень чтобы он менял его положение
+
+        this.gameObject.GetComponent<StoneInLiquid>().LiquidRenderer = CollisionObject.GetComponent<Renderer>(); // Получаем материал чтобы брать FluidAmmount из шеййдера
+
+        this.gameObject.transform.SetParent(CollisionObject.transform); // Ставим родительским объектом у камня жидкость чтобы сбросить координату на 0.
+
+        this.gameObject.GetComponent<BoxCollider>().enabled = false; // Выключаем коллицзию у камня чтобы он мог спокойно быть в колбе и не вылетать из нее
+        Destroy(this.gameObject.GetComponent<Rigidbody>()); // Уничтожаем Rigidbody тк нам не нужна физика чтобы он падал вниз
+
+        this.gameObject.transform.localPosition = Vector3.zero; // Ставим локальную позицию по 0
     }
 
     private void Update()
@@ -53,13 +82,28 @@ public class ElementIndification : MonoBehaviour
             ColorWithoutOxygen = this.gameObject.GetComponent<Renderer>().material.color;
             Debug.Log("Lithium - Oxygen");
         }
+
+
+        // Effects
+
+        if(Oxidation == true)
+        {
+            BeginLerp = this.gameObject.transform.localPosition;
+            FinishLerp = new Vector3(Random.Range(-0.005f, 0.005f), Random.Range(-0.005f, 0.005f), 0);
+
+
+            this.gameObject.transform.localPosition = Vector3.Lerp(BeginLerp, FinishLerp, 0.03f);
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag == "CanAddLiquid") // Если камень падает в жидкость
         {
-            LithiumInFluid();
+            CollisionObject = collision.collider.gameObject; // Присваеваем объект с которым столкнулся наш камень другому чтобы в дальнейшем это использовать
+
+            LithiumInFluid(); // Запускаем метод когда литий падает в воду
         }
     }
 }
---
+++ Game/Scripts/StoneInLiquid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneInLiquid : MonoBehaviour
+{
+    [Header("Объект который будет плавать в жидкости:")]
+    public GameObject Stone;
+    [Header("Шейдер жидкости:")]
+    public Renderer LiquidRenderer;
+    [Header("Число для управления заполненостью")]
+    [SerializeField] private float LiquidControll;
+    private void Update()
+    {
+        if (Stone != null && LiquidRenderer != null)
+        {
+            Vector3 BeginLerp = Stone.transform.localPosition;
+            Vector3 FinishLerp = new Vector3(Stone.transform.localPosition.x, Stone.transform.localPosition.y, Vector3.forward.y * 0.02f);
+
+            if (LiquidRenderer.material.GetFloat("_FillAmount") < 0.5f)
+            {
+                LiquidControll = 0.01f;
+                Stone.transform.localPosition = new Vector3(0, 0, LiquidRenderer.material.GetFloat("_FillAmount") * LiquidControll);
+                Stone.transform.localPosition = Vector3.Lerp(BeginLerp, FinishLerp, 0.1f);
+                Stone.transform.localPosition = Vector3.Lerp(FinishLerp, BeginLerp, 0.1f);
+            }
+            else if (LiquidRenderer.material.GetFloat("_FillAmount") == 0.5f)
+            {
+                Stone.transform.localPosition = new Vector3(0, 0, 0);
+                Stone.transform.localPosition = Vector3.Lerp(BeginLerp, FinishLerp, 0.1f);
+            }
+            else
+            {
+                LiquidControll = -0.01f;
+                Stone.transform.localPosition = new Vector3(0, 0, LiquidRenderer.material.GetFloat("_FillAmount") * LiquidControll);
+                Stone.transform.localPosition = Vector3.Lerp(BeginLerp, FinishLerp, 0.1f);
+                Stone.transform.localPosition = Vector3.Lerp(FinishLerp, BeginLerp, 0.1f);
+
+            }
+        }
+
+        Debug.Log(LiquidRenderer.material.GetFloat("_FillAmount"));
+    }
+}
